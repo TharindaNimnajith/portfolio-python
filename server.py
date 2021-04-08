3adb0b2d8848c4771254e44662d20818c985f052
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-08 13:22:50 +0530

    portfolio

---
+++ server.py
@@ -0,0 +1,82 @@
+import os
+
+from flask import Flask, render_template, request, redirect
+
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# @app.route('/index.html')
+# def index():
+#     return render_template('index.html')
+#
+#
+# @app.route('/work.html')
+# def work():
+#     return render_template('work.html')
+#
+#
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+#
+#
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+#
+#
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+#
+#
+# @app.route('/components.html')
+# def components():
+#     return render_template('components.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        database.write(f'{email}, {subject}, {message}\n')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        write_to_csv(data)
+        return redirect('thank_you.html')
+    else:
+        return 'error'
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
